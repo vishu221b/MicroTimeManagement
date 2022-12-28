1b8d613f5a00408704310225cf596102ff606e6a
vishu221b <vishal.dogra738@gmail.com>
2022-12-28 13:21:49 +1000

    Dockerfile, Environment specific Config, prod value based config, Security config Constructor Injection

--- backend/api-service/src/main/java/com/microtimemanagement/apiservice/config/SecurityConfig.java
+++ backend/api-service/src/main/java/com/microtimemanagement/apiservice/config/SecurityConfig.java
@@ -1,9 +1,10 @@
 package com.microtimemanagement.apiservice.config;
 
 import com.microtimemanagement.apiservice.filter.MtmSessionFilter;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.core.GrantedAuthorityDefaults;
@@ -13,22 +14,19 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
 
-    @Autowired
-    MtmSessionFilter mtmSessionFilter;
-    public static final Integer BCRYPT_STRENGTH = 10; //default
+    private final MtmSessionFilter mtmSessionFilter;
 
-    /**
-     * For custom prefixing to all roles
-     * */
     @Bean
     public static GrantedAuthorityDefaults authorityDefaults(){
         return new GrantedAuthorityDefaults("_");
     }
 
     @Bean
-    SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+    @Profile("dev")
+    SecurityFilterChain devSecurityFilterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity
                 .httpBasic().disable().csrf().disable().cors()
                 .and()
@@ -40,8 +38,33 @@ public class SecurityConfig {
                                         "/api/v1/auth/login",
                                         "/actuator/**",
                                         "/error",
-                                        "/api/v1/users/register"
+                                        "/api/v1/users/register",
+                                        "/",
+                                        "/swagger-dev"
                                 ).permitAll()
+                                .requestMatchers("/api/v1/admin/**").hasRole("MTM_ADMIN")
+                                .requestMatchers("/api/v1/**", "api/v1/auth/logout").hasRole("MTM_USER")
+                ).authorizeHttpRequests().anyRequest().denyAll()
+                .and().addFilterBefore(mtmSessionFilter, UsernamePasswordAuthenticationFilter.class)
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and().build();
+    }
+
+    @Bean
+    @Profile("prod")
+    SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .httpBasic().disable().csrf().disable().cors()
+                .and()
+                .authorizeHttpRequests(
+                        auth -> auth
+                                .requestMatchers(
+                                        "/api/v1/users/register",
+                                        "/api/v1/auth/login",
+                                        "/error"
+                                ).permitAll()
+                                .requestMatchers("/", "/actuator/**").hasRole("MTM_ACTUATOR")
+                                .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").hasRole("MTM_SWAGGER")
                                 .requestMatchers("/api/v1/admin/**").hasRole("MTM_ADMIN")
                                 .requestMatchers("/api/v1/**", "api/v1/auth/logout").hasRole("MTM_USER")
                 ).authorizeHttpRequests().anyRequest().denyAll()
